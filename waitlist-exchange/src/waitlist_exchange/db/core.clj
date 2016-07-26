f2b2e1b4eadf8eaead11cbab1e251b750dee85ea
hemangandhi <hemangandhi@gmail.com>
2016-07-26 18:27:10 -0400

    auth started

--- waitlist-exchange/src/waitlist_exchange/db/core.clj
+++ waitlist-exchange/src/waitlist_exchange/db/core.clj
@@ -1,16 +1,16 @@
 (ns waitlist-exchange.db.core
-  (:import [java.io PushbackReader]
-           [clojure.java.io reader]))
+  (:import [java.io PushbackReader])
+  (:require [clojure.java.io :as io]))
 
 (defn read-from-file [filename]
-  (with-open [rdr (PushbackReader. (reader filename))]
+  (with-open [rdr (PushbackReader. (io/reader filename))]
     (binding [*read-eval* false]
       (read rdr))))
 
 (defn write-to-file [filename data]
   (spit filename (pr data)))
 
-(def default-path "/db/")
+(def default-path (io/resource "db/"))
 
 (defn write-user-info [info]
   (write-to-file (str default-path "user-db.edn")))
--- waitlist-exchange/src/waitlist_exchange/server/core.clj
+++ waitlist-exchange/src/waitlist_exchange/server/core.clj
@@ -1,16 +1,40 @@
 (ns waitlist-exchange.server.core
   (:require [cemerick.friend :as friend]
+            [waitlist-exchange.db.user-db :as wle-udb]
             [cemerick.friend.workflows :refer (make-auth)]
             [cemerick.friend.credentials :as creds]
             [compojure.core :refer (GET POST routes defroutes)]
             [compojure.handler :refer (site)]
             [ring.util.response :as resp]
             [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.session :refer [wrap-session]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
             [waitlist-exchange.html.core :as wle-html]))
       
 (defroutes main-app
   (GET "/" req (wle-html/gen-page false ""))
-  (GET "/utest" req (wle-html/gen-page true "Tester")))
+  (GET "/utest" req (friend/authorize #{::user} 
+                                      (wle-html/gen-page true 
+                                                         (get-in (friend/current-authentication req) 
+                                                                 [:user :name]))))
+  (friend/logout (GET "/logout" [] (wle-html/gen-page false ""))))
+
+(defn wle-cred-fn [{:keys [email password] :as creds}]
+  (when-let [usr (@wle-udb/users email)]
+    (when (= (creds/hash-bcrypt password) (usr :password))
+      {:identity email :roles #{::user} :user usr})))
+
+(defn wle-auth-wkflow [req]
+  (when (and (= (:request-method req) :post)
+             (= (:uri req) "/login"))
+    (let [cred-fn (get-in req [::friend/auth-config :credential-fn])]
+      (make-auth (cred-fn (select-keys (:params req) [:email :password]))))))
 
 (defn start-srv []
-  (run-jetty main-app {:port 8080}))
+  (run-jetty (-> main-app
+                 (friend/authenticate {:workflows [wle-auth-wkflow]
+                                       :credential-fn wle-cred-fn})
+                 (wrap-keyword-params)
+                 (wrap-params)
+                 (wrap-session)) {:port 8080}))
