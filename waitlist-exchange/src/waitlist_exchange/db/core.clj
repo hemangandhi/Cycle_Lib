73fa7ea846370ff3b00aa46b3e6f3a14e8267ca6
Heman Gandhi <hemangandhi@gmail.com>
2016-08-26 15:13:57 -0400

    Start of adding desires

--- waitlist-exchange/src/waitlist_exchange/db/core.clj
+++ waitlist-exchange/src/waitlist_exchange/db/core.clj
@@ -1,6 +1,8 @@
 (ns waitlist-exchange.db.core
   (:require [clojurewerkz.neocons.rest :as nr]
             [clojure.java.io :as io]
+            [clojure.string :as clj-str]
+            [clojurewerkz.neocons.rest.relationships :as nrl]
             [clojurewerkz.neocons.rest.cypher :as cy]
             [clojurewerkz.neocons.rest.labels :as lbls]
             [clojurewerkz.neocons.rest.nodes :as nn]))
@@ -15,9 +17,17 @@
 (defn internalize [key-prefix hashmap]
   (alter-keys hashmap #(keyword (.replace % key-prefix ""))))
 
+(defn fudge-id-attr [raw-node-info]
+  (assoc raw-node-info :id (get-in raw-node-info [:metadata :id])))
+
+(defn users-raw [email]
+  (let [res (cy/query neo4j-conn (str "MATCH (val:User) WHERE val.email = \"" email "\" RETURN val"))]
+    (if (cy/empty? res) nil res)))
+
 (defn users [email]
-  (let [res (cy/tquery neo4j-conn (str "MATCH (val:User) WHERE val.email = \"" email "\" RETURN val.email, val.name, val.password"))]
-    (if (empty? res) nil (internalize "val." (first res)))))
+  (let [raw (users-raw email)]
+    (if (nil? raw) nil
+      (get-in raw [:data 0 0 :data]))))
 
 (defn add-user [name email pass]
   (if (not (nil? (users email)))
@@ -25,3 +35,20 @@
     (let [node (nn/create neo4j-conn {:name name :email email :password pass})]
       (lbls/add neo4j-conn node "User")
       (:data node))))
+
+(defn get-or-make-desire [have-name have-ind want-name want-ind]
+  (let [node-obj {:haveName have-name :haveInd have-ind :wantName want-name :wantInd want-ind}
+        res (cy/query neo4j-conn (str "MATCH (v:Desire) WHERE " 
+                                      (clj-str/join ", " (map (fn [[k v]] (str "v." k " = {" v "}")) node-obj))
+                                      " RETURN v") node-obj)]
+    (if (cy/empty? res) 
+      (let [new-node (nn/create neo4j-conn node-obj)]
+        (lbls/add neo4j-conn new-node "Desire")
+        new-node)
+      (ffirst (:data res)))))
+
+(defn add-user-desire [email [have-name have-ind want-name want-ind :as desire-info]]
+  (let [user-node (fudge-id-attr (users-raw email))
+        desire-node-tmp (apply get-or-make-desire desire-info)
+        desire-node (if (contains? desire-node-tmp :id) desire-node-tmp (fudge-id-attr desire-node-tmp))]
+    (nrl/create user-node desire-node :Desires)))
